80f1510d044e4402941c165f38972445d909243b
seon1206 <127457404+seon1206@users.noreply.github.com>
2024-04-23 08:12:24 +0000

    lesson11 끝!!

--- lesson-11/controllers/homeController.js
+++ lesson-11/controllers/homeController.js
@@ -4,14 +4,51 @@
 /**
  * Listing 10.2
  */
-exports.respondWithName = (req, res) => {
+exports.respondWithName1 = (req, res) => {
     res.render('index');
 }
 
 /**
  * Listing 10.3
  */
-exports.respondWithName2 = (req, res) => {
-    let paramsName = req.params.myName;
-    res.render('index', { name: paramsName });
+exports.getHome = (req, res) => {
+    res.render('index', {
+        name : "PaRk",
+        people: {
+            usa: {
+                einstein: "Albert Einstein",
+                musk: "Elon Musk",
+                jobs: "Steve Jobs"
+            },
+            korea : {
+                psy: "박지상",
+                kim: "김연아"
+            },
+            mexico: {
+                jesus: "Jesus Alvarez"
+            }
+        }
+    });
+} // "/" index
+// exports.getContact = (req, res) => {} // "/contact" GET method
+// exports.postContact = (req, res) => {} // "/contact" POST method
+
+exports.respondWithName = (req, res) => {
+    let paramsName = req.params.myName; // 라우트의 매개변수 /name/:myName
+    res.render('index', {
+        name: paramsName,
+    people: {
+        usa: {
+            einstein: "Albert Einstein",
+            musk: "Elon Musk",
+            jobs: "Steve Jobs"
+        },
+        korea : {
+            psy: "박지상",
+            kim: "김연아"
+        },
+        mexico: {
+            jesus: "Jesus Alvarez"
+        }
+    } });
 };
--- lesson-11/main.js
+++ lesson-11/main.js
@@ -1,4 +1,36 @@
 // main.js
 "use strict";
 
+const expressEjsLayouts = require('express-ejs-layouts');
+
 // @TODO: 웹 서버를 만들고 실행한다.
+const port = 3000,
+express = require('express'),
+layouts = require('express-ejs-layouts'),
+homeController = require("./controllers/homeController"),
+errorController = require("./controllers/errorController"),
+app = express();
+
+// BEST 습관 : 2단계 앱 설정 (set함수, 그 다음에 use 함수)
+app.set("port", process.env.PORT || port);
+app.set("view engine", "ejs")
+
+app.use(layouts);
+app.use(express.static("public"));
+
+// Aaron의 습관 : GET 라우트 먼저 그다음에 POST
+app.get("/", homeController.getHome);
+app.get("/name/:myName", homeController.respondWithName);
+app.get("/test", (req, res) => {
+    res.sendFile("./public/test.html")
+});
+
+// BEST 습관 : 순서 때문에 errorController app.use()는 거의 마지막 (listen 전)
+app.use(errorController.logErrors);
+app.use(errorController.resNotFound); //404
+app.use(errorController.resInternalError); //500
+
+// BEST 습관 : 마지막 listen 함수
+app.listen(app.get("port"), () => {
+    console.log(`server at: http://localhost:${app.get("port")}`);
+})
